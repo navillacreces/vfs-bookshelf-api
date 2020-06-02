eff4967e0d07de9edd4556cb91f234592aa2c713
Nicholas Villacreces <nvillacreces72@gmail.com>
2020-06-02 18:15:17 -0500

    bookService

--- src/books/booksService.js
+++ src/books/booksService.js
@@ -1,5 +1,25 @@
 const booksService = {
 
+
+    getAllBooks(knex){
+        return knex.select('*').from('bookshelf');
+    },
+
+    insertBook(knex,newBook){
+        return knex
+        .insert(newBook)
+        .into('bookshelf')
+        .returning('*')
+        .then(rows =>{
+                return rows[0]
+        })
+    },
+
+    deleteRecipe(knex, id ){
+        return knex('bookshelf').where({id}).delete()
+    }
+
+
 }
 
 module.exports = booksService

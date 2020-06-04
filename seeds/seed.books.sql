a66cd8ad5e7067a80ceb555085b28709895a0b0f
Nicholas Villacreces <nvillacreces72@gmail.com>
2020-06-03 19:14:09 -0500

    updated seed

--- seeds/seed.books.sql
+++ seeds/seed.books.sql
@@ -1,46 +1,52 @@
 BEGIN;
 
-INSERT INTO bookshelf (title,author,rating,ownership,img,purchase_link)
+INSERT INTO bookshelf (title,author,rating,ownership,isbn,img,purchase_link)
 
 VALUES
 (
 'Adam Carolla',
 'Not Taco Bell Material',
 '4',
-'Not Owned',
-'http://books.google.com/books/content?id=PQGB6BWJTJ8C&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
-'http://books.google.com/books?id=PQGB6BWJTJ8C&printsec=frontcover&dq=inauthor:Carolla+intitle:Not+Taco+Bell&hl=&cd=1&source=gbs_api'
+'Not Yet Owned',
+'9780307888877',
+'http://books.google.com/books/content?id=yRw0Ih18ZBEC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+'http://books.google.com/books?id=yRw0Ih18ZBEC&printsec=frontcover&dq=inauthor:Carolla+isbn:9780307888877&hl=&cd=1&source=gbs_api'
 ),
 (
 'Chuck Palahniuk',
 'Fight Club: A Novel',
 '4',
 'Hardcopy',
-'http://books.google.com/books/content?id=hoGkPfds4tAC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
-'http://books.google.com/books?id=hoGkPfds4tAC&printsec=frontcover&dq=inauthor:Palahniuk+intitle:Fight+Club+A&hl=&cd=1&source=gbs_api'
+'9780393039764',
+'http://books.google.com/books/content?id=-qgAgg91C9wC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+'http://books.google.com/books?id=-qgAgg91C9wC&printsec=frontcover&dq=inauthor:Palahniuk+isbn:9780393039764&hl=&cd=1&source=gbs_api'
 ),
 (
 'Aldous Huxley',
 'Brave New World',
 '3',
 'E-Book',
-'http://books.google.com/books/content?id=niDNtZoYsAUC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
-'http://books.google.com/books?id=niDNtZoYsAUC&printsec=frontcover&dq=inauthor:Huxley+intitle:Brave&hl=&cd=1&source=gbs_api'
+'9783150092842',
+'http://books.google.com/books/content?id=mcyVPwAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
+'http://books.google.com/books?id=mcyVPwAACAAJ&dq=inauthor:Huxley+isbn:9783150092842&hl=&cd=1&source=gbs_api'
 ),
 (
 'Walter Isaacson',
 'Steve Jobs',
 '4',
 'Hardcopy',
-'http://books.google.com/books/content?id=6e4cDvhrKhgC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
-'http://books.google.com/books?id=6e4cDvhrKhgC&printsec=frontcover&dq=inauthor:Isaacson+intitle:Steve&hl=&cd=1&source=gbs_api'
+'9781408703748',
+'http://books.google.com/books/content?id=BYy1swEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api',
+'http://books.google.com/books?id=BYy1swEACAAJ&dq=inauthor:Isaacson+isbn:9781408703748&hl=&cd=1&source=gbs_api'
 ),
 (
 'Hunter S. Thompson',
 'The Rum Diary',
 '3',
-'Not Owned',
-'http://books.google.com/books/content?id=0AuXF547DqoC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
-'http://books.google.com/books?id=0AuXF547DqoC&printsec=frontcover&dq=inauthor:Thompson+intitle:The+Rum&hl=&cd=1&source=gbs_api');
+'Not Yet Owned', 
+'9781451659719',
+'http://books.google.com/books/content?id=c5tMZGbhCkEC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
+'http://books.google.com/books?id=c5tMZGbhCkEC&printsec=frontcover&dq=inauthor:Thompson+isbn:9781451659719&hl=&cd=1&source=gbs_api'
+);
 
 COMMIT;
